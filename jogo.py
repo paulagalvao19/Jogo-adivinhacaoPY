91e82514621fc7a49fe8392377c3a680799218b0
Paula Galvão <102521290+paulagalvao19@users.noreply.github.com>
2024-12-20 23:09:29 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,9 +1,9 @@
 import random
 
-numero_secreto = random.randint(1, 100)
+numero_secreto = random.randint(1, 100) # Definindo o numero aleatorio
 
 
-def acertou(numEscolhido):
+def acertou(numEscolhido): 
     if numero_secreto == numEscolhido:
         return 'Você acertou! Parabéns!!'
     elif numero_secreto < numEscolhido:
@@ -16,9 +16,9 @@ def escolha():
     print('Bem-vindo ao Modo Difícil do Jogo de Adivinhação!')
     print('Você tem 10 tentativas para adivinhar o número secreto entre 1 e 100.')
 
-    tentativas = 0
+    tentativas = 0 
 
-    while tentativas < 10:
+    while tentativas < 10: # Maximo de tentativa
         try:
             numEscolhido = int(input('Digite sua tentativa: '))
             tentativas += 1
@@ -30,10 +30,10 @@ def escolha():
                 print(f'Você precisou de {tentativas} tentativa(s).')
                 break
         except ValueError:
-            print('Por favor, insira um número válido!')  # Mensagem amigável para entrada inválida
+            print('Por favor, insira um número válido!') 
 
 
     if tentativas == 10 and numero_secreto != numEscolhido:
-        print(f'Você perdeu! O número secreto era {numero_secreto}. Tente novamente!')
+        print(f'Você perdeu! O número secreto era {numero_secreto}. Tente novamente!') # Se o usuário ultrapassou o limite 
 
 escolha()
