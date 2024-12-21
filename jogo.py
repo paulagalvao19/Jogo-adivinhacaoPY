434ac17e8fb83d45ad7e53624bca974a3c26fcac
paulagalvao19 <paulagalvao1903@outlook.com>
2024-12-20 22:48:22 -0300

    Primeiro commit: Adicionado jogo de adivinhação

---
+++ jogo.py
@@ -0,0 +1,39 @@
+import random
+
+numero_secreto = random.randint(1, 100)
+
+
+def acertou(numEscolhido):
+    if numero_secreto == numEscolhido:
+        return 'Você acertou! Parabéns!!'
+    elif numero_secreto < numEscolhido:
+        return 'Você errou! Tente um número menor...'
+    else:
+        return 'Você errou! Tente um número maior...'
+
+
+def escolha():
+    print('Bem-vindo ao Modo Difícil do Jogo de Adivinhação!')
+    print('Você tem 10 tentativas para adivinhar o número secreto entre 1 e 100.')
+
+    tentativas = 0
+
+    while tentativas < 10:
+        try:
+            numEscolhido = int(input('Digite sua tentativa: '))
+            tentativas += 1
+
+            resultado = acertou(numEscolhido)
+            print(resultado)
+
+            if numero_secreto == numEscolhido:
+                print(f'Você precisou de {tentativas} tentativa(s).')
+                break
+        except ValueError:
+            print('Por favor, insira um número válido!')  # Mensagem amigável para entrada inválida
+
+
+    if tentativas == 10 and numero_secreto != numEscolhido:
+        print(f'Você perdeu! O número secreto era {numero_secreto}. Tente novamente!')
+
+escolha()
